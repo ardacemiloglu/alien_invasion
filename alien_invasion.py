e2ac1774cd741b9dc4bbae143495d70cf0431b3d
ardacemiloglu <carda2002@gmail.com>
2025-12-24 17:19:02 +0100

    frog added

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from frog import Frog
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,6 +19,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.frog = Frog(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -37,6 +39,7 @@ class AlienInvasion:
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        self.frog.blitme()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
--- frog.py
+++ frog.py
@@ -1,19 +1,18 @@
 import pygame
 
-class Ship:
-    """A class to manage the ship."""
-
+class Frog:
+    """A class to manage the frog."""
     def __init__(self, ai_game):  #ai_game is passed to INHERIT the attributes like the screen rectangle etc.
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('alien_invasion\images\ship.bmp')
+        self.image = pygame.image.load('alien_invasion/images/240_bitmap.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.center = self.screen_rect.center
 
     def blitme(self):
         """Draw the ship at its current location."""
