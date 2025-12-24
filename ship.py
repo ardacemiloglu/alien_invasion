cb75c67a8cd320c42788f613e82102b9348b0836
ardacemiloglu <carda2002@gmail.com>
2025-12-24 19:26:54 +0100

    speed setting added

--- ship.py
+++ ship.py
@@ -7,8 +7,10 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         self.moving_right = False
+        self.moving_left = False
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('alien_invasion\images\ship.bmp')
@@ -17,10 +19,19 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+        
     def update(self):
         """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        elif self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
